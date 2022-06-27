04a0fdb8bfbc76804c7d9b3ee8f3e7c3d187d4a0
chvark <ivanchvark@gmail.com>
2022-06-27 16:50:07 +0400

    Collapsed 14 problem into one method.

--- 14-longest-common-prefix.js
+++ 14-longest-common-prefix.js
@@ -3,26 +3,20 @@
  * @return {string}
  */
 const longestCommonPrefix = strs => {
-    let result = strs[0];
-    for (let i = 1; i < strs.length; i++) {
-        result = intersection(result, strs[i]);
-        if (!result) return "";
+    let result = strs[0], i = 1;
+    while (i < strs.length) {
+        let concatedPerfix = "";
+        const str1 = strs[i];
+        const loopIterNumber = str1.length > result.length ? str1.length : result.length;
+        for (let j = 0; j < loopIterNumber; j++) {
+            const symbolToConcat = str1[j] === result[j] ? str1[j] : "";
+            if (!symbolToConcat) break;
+            concatedPerfix = concatedPerfix.concat(symbolToConcat);
+        }
+        if (!concatedPerfix) return "";
+
+        result = concatedPerfix;
+        i++;
     }
     return result;
-}
-
-/**
- * @param {string} str1
- * @param {string} str2
- * @return {string}
- */
-const intersection = (str1, str2) => {
-    let res = "";
-    const loopIterNumber = str1.length > str2.length ? str1.length : str2.length;
-    for (let i = 0; i < loopIterNumber; i++) {
-        const symbolToConcat = str1[i] === str2[i] ? str1[i] : null;
-        if (!symbolToConcat) break;
-        res = res.concat(symbolToConcat);
-    }
-    return res;
 }
