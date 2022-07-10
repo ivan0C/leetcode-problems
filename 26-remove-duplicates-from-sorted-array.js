c2ecd40c6c96adb55cc99d508248f3fefb623def
chvark <ivanchvark@gmail.com>
2022-07-11 01:47:54 +0400

    Get an result faster than 97% of users for task #26

--- 26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array.js
@@ -1,35 +1,22 @@
 /**
  * @param {number[]} nums
- * @returns {number[]} 
+ * @returns {number} 
 **/
 const removeDuplicates = (nums) => { 
     if (!nums.length || nums.length === 1) return nums;
-    let prevElem;
-    let markToDelete = false;
-    
-    for (let i = 0; i < nums.length; i++){
-        if (!i) {
-            prevElem = nums[i];
-            
+    let i = 1;
+    let undefCount = 0;
+    while (true) {
+        if (i > nums.length - 1) {
+            break;
         }
-        else if (markToDelete) {
-            prevElem = nums[i];
-            nums[i-1] = nums[i];
-            nums[i] = undefined;
-            markToDelete = false;
-            
+        else if (nums[i - 1] === nums[i]) {
+            nums[i] = nums[i - 1];
+            nums[i - 1] = undefined;
+            undefCount++;
         }
-        else if (prevElem === nums[i]) {
-            prevElem = nums[i];
-            nums[i] = undefined;
-            markToDelete = true;
-            
-        }
-        else {
-            prevElem = nums[i];
-        }
-    }
-    return nums;
-}
-
-console.log(removeDuplicates([1, 2, 2, 3, 3, 4, 5, 6, 6, 7]));
+        i++;
+    };
+    nums.sort((a, b) => a - b);
+    return nums.length - undefCount;
+};
