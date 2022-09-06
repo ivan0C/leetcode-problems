597e892a2bb72722cf84c19883238588d59ecf19
Chvarkou <ivan.chvarkou@sennder.com>
2022-09-06 23:06:01 +0400

    Added iterable solution for task 206.

--- 206-reverse-linked-list.js
+++ 206-reverse-linked-list.js
@@ -9,7 +9,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function (head) {
+var reverseListRecursice = function (head) {
     if (!head || !head.next) return head;
     const giveNewNode = (curHead, headToStore) => {
         headToStore.val = curHead.val;
@@ -25,4 +25,32 @@ var reverseList = function (head) {
         )
     }
     return giveNewNode(head, new ListNode());
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    if (!head || !head.next) return head;
+    
+    let newHead = new ListNode();
+
+    while (true) {
+        newHead.val = head.val;
+        if (!head.next) {
+            break;
+        }
+        newHead = new ListNode(0, newHead);
+        head = head.next;
+    }
+    return newHead
 };
