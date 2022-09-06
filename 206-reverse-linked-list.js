da6b2e8e04e6378c405bea628ab420831a1f5392
Chvarkou <ivan.chvarkou@sennder.com>
2022-09-06 23:01:04 +0400

    Added recursive solution for task 206.

---
+++ 206-reverse-linked-list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    if (!head || !head.next) return head;
+    const giveNewNode = (curHead, headToStore) => {
+        headToStore.val = curHead.val;
+
+        if (!curHead.next) {
+            return headToStore;
+        }
+        return giveNewNode(
+            curHead.next, new ListNode(
+                0,
+                headToStore
+            )
+        )
+    }
+    return giveNewNode(head, new ListNode());
+};
